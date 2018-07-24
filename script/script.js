df4fd936a224f59f5538608deaeadf49a6fdcd4a
adarsh-ms <35689286+adarsh-ms@users.noreply.github.com>
2018-07-24 22:13:28 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,26 +1,34 @@
-$(document).ready(function() {
+function myFunction() 
+{
+	var x = document.getElementById("navigation");
+	if (x.className === "navigate brackets") 
+	{
+	    x.className += " responsive";
+	} 
+	else 
+	{
+	    x.className = "navigate brackets";
+	}
+}
+
+
+
+$(document).ready(function() 
+{
 
   	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   	particlesJS.load('particles-js', 'script/particlesjs-config.json', function() {
   	console.log('callback - particles.js config loaded');});
 
-  	function myFunction() 
-  	{
-  		var x = document.getElementById("navigation");
-		if (x.className === "navigate brackets") 
-		{
-		    x.className += " responsive";
-		} 
-		else 
-		{
-		    x.className = "navigate brackets";
-		}
-	}
-
-	// $('#navigation.icon').on('click','.icon',function(){
-
-	// 	myFunction();
-	// });
-
 });
 
+// function changePage()
+// {
+//    console.log("change_page");
+//    $('body').load("about.html");
+// } 
+
+// $('#about').on('click',function(){
+
+	// 	changePage();
+	// });
